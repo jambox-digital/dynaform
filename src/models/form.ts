958d7d4fcc99c02af354fe1235205571561e8538
Ivan Pozderac <ivan.pozderac@gmail.com>
2020-09-14 21:23:58 +0100

    extend model for form with prototype interface

--- src/models/form.ts
+++ src/models/form.ts
@@ -33,10 +33,75 @@ export interface IInput {
     dirname: string, // submits the direction of text
     disabled: boolean, // all types
     form: string, // form id for elements outside the form, but part of the form
-    
+    formaction: string, //URL for submit | image
+    formenctype: string, //text/plain, multipart/form-data
+    formmethod: string, // get | post
+    formnovalidate: boolean, // no validation on submit
+    formtarget: string, // _blank ... 
+    height: number, //pixels
+    list: string, //datalist_id
+   // max: number, //number | date
+    maxlength: number, //number
+   // min: number, // number
+    minlength: number,
+    multiple: boolean, //multiple value for input element
+    name: string, //for text
+    pattern: string, //regExp
+   // placeholder: string,
+    readonly: boolean,
+   // required: boolean,
+    size: number, //number of charachters in input
+    src: string, //URL of the image for submit button
+   // step: number, //interval between 2 steps
+   // type: string, //all supported types
+    value: string, // for text input
+   // width: number //in pixels
 }
 
 export interface IFormObject {
     inputs: IInput[]
     //buttons: IButtons
+}
+
+
+export interface IInputTextPrototype {
+    input: {
+        alt?: string, // type=text
+        autocomplete?: string, // on (default) | off - could be boolean types: text search, url, tel, email, password, datepickers, range, color
+        autofocus?: boolean, //no value, so boolean, 
+        dirname?: string, // submits the direction of text
+        disabled?: boolean, // all types
+        form?: string, // form id for elements outside the form, but part of the form
+        formaction?: string, //URL for submit | image
+        formenctype?: string, //text/plain, multipart/form-data
+        formmethod?: string, // get | post
+        formnovalidate?: boolean, // no validation on submit
+        formtarget?: string, // _blank ... 
+        height?: number, //pixels
+        id?: string, //id
+        list?: string, //datalist_id
+        name?: string, //for text
+        pattern?: string, //regExp
+        placeholder?: string,
+        readonly?: boolean,
+        required?: boolean,
+        size?: number, //number of charachters in input
+        type: string, //all supported types
+        value?: string, // for text input
+        width?: number //in pixels
+    },
+    dynaform: {
+        onChange?: () => void,
+        defaultValue: string | number,
+        defaultNumericValue?: number | number[] 
+        label: string,
+        iconComponent?: React.ElementType,
+        onBlur?: (event: Event, data: any) => void;
+    },
+    style: {
+        color?: string,
+    },
+    validation: {
+        
+    }
 }
